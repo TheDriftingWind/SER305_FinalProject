cbd3bf07a6f0f9a0fdd024cba5b5dece82a75961
TheDriftingWind <CharlesYZhu@yahoo.com>
2017-11-11 14:54:12 -0500

    v0.0

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//init
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter wait:");
+		int wait = scan.nextInt();
+		System.out.println("Enter keyword:");
+		String keyword = scan.nextLine();
+		
+		
+		Document page = Jsoup.connect("https://dealsea.com/").timeout(6000).get();
+		
+		
 
 	}
 
