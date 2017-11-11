553c51a11e3317474065c68c69f4fa76f52fb15d
TheDriftingWind <CharlesYZhu@yahoo.com>
2017-11-11 16:12:58 -0500

    testing

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,35 @@ import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		//init
+	public static void main(String[] args) throws IOException, InterruptedException {
 		Scanner scan = new Scanner(System.in);
+		//enter wait time
 		System.out.println("Enter wait:");
 		int wait = scan.nextInt();
+		System.out.println(wait);
+		//enter keyword
 		System.out.println("Enter keyword:");
 		String keyword = scan.nextLine();
+		System.out.println(keyword);
 		
+		//loop
+		boolean loop = true;
+		String stop = scan.nextLine();
 		
 		Document page = Jsoup.connect("https://dealsea.com/").timeout(6000).get();
-		
+		Elements div = page.select("div");
+		while(loop){
+			System.out.println(stop);
+			if(stop == "stop"){
+				loop = false;
+			}
+			
+			Thread.sleep(wait);
+		}
 		
 
 	}
