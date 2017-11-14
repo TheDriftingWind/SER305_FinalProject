e8fec848f2ab7b327d7ee56ed5088adb369d37fb
Charles Zhu <CharlesYZhu@yahoo.com>
2017-11-13 19:44:54 -0500

    part 4 ready for merge

--- src/ActionTimer.java
+++ src/ActionTimer.java
@@ -1,7 +1,13 @@
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.Timer;
+import javax.swing.filechooser.FileSystemView;
 
 public class ActionTimer extends Timer{
 	
@@ -31,7 +37,33 @@ public class ActionTimer extends Timer{
 			//3.) return the matching results
 			
 			//4.) open a new pop-up with the results
+			//get the desktop directory
+			File home = FileSystemView.getFileSystemView().getHomeDirectory();
+			//make the url where the html document will be created
+			String url = home.getAbsolutePath();
+			url = url + "/test.html";
 			
+			//For testing purposes
+			System.out.println(url); 
+			String html = "<!DOCTYPE html><html><head><title>Test</title></head><body><a href='https://www.google.com'>Link</a></body></html>";
+			
+			//create new file
+			File f = new File(url);
+			//write to the html file
+			try {
+				BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+				bw.write(html);
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			//open the file for the user to see
+			try {
+				Desktop.getDesktop().browse(f.toURI());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
